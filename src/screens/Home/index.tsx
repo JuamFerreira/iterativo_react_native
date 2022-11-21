fd8633531a8b5a4c1943674adaeaf97c38f3e621
juam Ferreira <juamferreira.c@gmail.com>
2022-11-21 17:36:15 -0300

    [04] Criando projeto TypeScript

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+export function Home(){
+  return (
+	<View style={{jusyifyContent: 'center', alignItems: 'center'}}>
+		<Text>Home</Text>
+	</View>
+  )
+}
